790aa2ab7aeaf79ea9ce0e82b45c4d5c2828fd26
Davide Di Criscito <dcdavidev@outlook.com>
2025-09-12 22:23:03 +0200

    chore: add dev tools

---
+++ prettier.config.mjs
@@ -0,0 +1,11 @@
+import telarethPrettierConfig from '@telareth/prettier-config';
+
+/**
+ * @see https://prettier.io/docs/configuration
+ * @typedef {import("prettier").Config}
+ */
+const config = {
+  ...telarethPrettierConfig,
+};
+
+export default config;
