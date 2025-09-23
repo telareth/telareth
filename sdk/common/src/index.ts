96791b23898770c3ec9fc89d1d9106960fce1109
Davide Di Criscito <davidedc@outlook.com>
2025-09-23 09:25:46 +0200

    fix: move generateRandomName helper to @telareth/common. Address #2

--- sdk/common/src/index.ts
+++ sdk/common/src/index.ts
@@ -1,2 +1,5 @@
+export * from './helpers/generate-random-name.js';
+export * from './helpers/parse-name.js';
 export * from './schemas/express.js';
+export * from './schemas/name.js';
 export * from './schemas/unions.js';
--- sdk/server/src/schemas/index.ts
+++ sdk/server/src/schemas/index.ts
@@ -3,6 +3,5 @@ export * from './cors.js';
 export * from './helmet.js';
 export * from './http-logger.js';
 export * from './logger.js';
-export * from './name.js';
 export * from './port.js';
 export * from './rate-limiter.js';
